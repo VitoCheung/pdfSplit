7b835dbcd1ea05b6fa297c7393042899a403db79
Vito_Cheung <34431336+VitoCheung@users.noreply.github.com>
2025-03-20 23:25:39 +0800

    Update split.py
    PyPDF2 command change

--- split.py
+++ split.py
@@ -4,22 +4,22 @@
 # @Author  : mh
 
 import os
-from PyPDF2 import PdfFileReader, PdfFileWriter
+from PyPDF2 import PdfReader, PdfWriter
 import time
 
-INPUT_FILE = "test.pdf"   # 待分割文件
-OUTOUT_FILE_NAME = "split"  # 分割后的文件名
-SUBFILE_PAGE_NUM = 20  # 分割页数
+INPUT_FILE = "数据挖掘与应用（SAS）（张俊妮）.pdf"   # 待分割文件
+OUTOUT_FILE_NAME = "dataminingsplit"  # 分割后的文件名
+SUBFILE_PAGE_NUM = 70  # 分割页数
 
 
 def subsplit(a, reader, pageCount):
-    output = PdfFileWriter()
+    output = PdfWriter()
     outfile = OUTOUT_FILE_NAME + str(a) + ".pdf"  # 分割后的文件
 
     for ipage in range(SUBFILE_PAGE_NUM):
         kp = ipage + SUBFILE_PAGE_NUM * a
         if kp < pageCount:
-            output.addPage(reader.getPage(kp))
+            output.add_page(reader.pages[kp])
 
     # 写入到目标PDF文件
     outputStream = open(outfile, "wb")
@@ -28,8 +28,8 @@ def subsplit(a, reader, pageCount):
 
 
 def split():
-    reader = PdfFileReader(open(INPUT_FILE, "rb"))
-    pageCount = reader.getNumPages()
+    reader = PdfReader(open(INPUT_FILE, "rb"))
+    pageCount = len(reader.pages)
 
     subFileNum = (int)(pageCount / SUBFILE_PAGE_NUM)
     if pageCount % SUBFILE_PAGE_NUM > 0:
